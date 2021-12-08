6c06bfa599a8c308a47d175457160b06069fb350
yahya <yahya@DESKTOP-F887M62>
2021-12-08 19:20:56 +0200

    push exercise one

--- src/test/java/yahya/Demo.java
+++ src/test/java/yahya/Demo.java
@@ -2,5 +2,27 @@ package yahya;
 
 public class Demo {
 	
- 
+	public static void main(String[] args) throws InterruptedException {
+		
+		// set System property for chromedrive
+				System.setProperty("webdriver.chrome.driver", "C:\\Users\\yahya\\Desktop\\Selenium\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		
+		driver.get("https://demoqa.com/text-box");
+		
+		driver.findElement(By.id("userName")).sendKeys("Yahya Khshali");
+		driver.findElement(By.id("userEmail")).sendKeys("yahya985@gmail.com");
+		driver.findElement(By.id("currentAddress")).sendKeys("irvine, CA");
+		driver.findElement(By.id("permanentAddress")).sendKeys("Orange County");
+		
+		driver.findElement(By.partialLinkText("Submit")).click();
+		
+		
+		Thread.sleep(5000);
+		driver.quit();
+	}
 }
